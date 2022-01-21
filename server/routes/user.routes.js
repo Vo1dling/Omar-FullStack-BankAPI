870c36a35ece1d10a812c537bcfbd92aa4970ef0
Vo1dling <attlanticazz@hotmail.com>
2022-01-21 18:56:02 +0200

    added routes and finished the utils functions

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const {
+  getUsers,
+  getUser,
+  postUser,
+  doAction,
+} = require("../controllers/user.controllers");
 
 const userRouter = express.Router();
+
+userRouter.get("/users/:id", getUser);
+userRouter.get("/users", getUsers);
+userRouter.post("/users", postUser);
+userRouter.put("/users/:action/:id", doAction);
+
+module.exports = userRouter;
